1a521520fe88eec1f569a632d7b444e39af7c7c4
Carlisle Hughes <22939626+carlislehughes@users.noreply.github.com>
2021-10-23 19:49:47 -0700

    Animating a model with the Animator

--- Assets/Scripts/Forest/NavPlayerMovement.cs
+++ Assets/Scripts/Forest/NavPlayerMovement.cs
@@ -9,6 +9,7 @@ public class NavPlayerMovement : MonoBehaviour
     Rigidbody rgBody = null;
     float trans = 0;
     float rotate = 0;
+    private Animator anim;
 
     public delegate void DropHive(Vector3 pos);
     public static event DropHive DroppedHive;
@@ -16,6 +17,7 @@ public class NavPlayerMovement : MonoBehaviour
     private void Start()
     {
         rgBody = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
     void Update()
     {
@@ -28,6 +30,8 @@ public class NavPlayerMovement : MonoBehaviour
         // The value is in the range -1 to 1
         float translation = Input.GetAxis("Vertical");
         float rotation = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", translation);
 
         trans += translation;
         rotate += rotation;
@@ -44,4 +48,13 @@ public class NavPlayerMovement : MonoBehaviour
         rgBody.velocity = move * speed * Time.deltaTime;
         trans = 0;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Hazard"))
+        {
+            anim.SetTrigger("Die");
+        }
+    }
 }
+
